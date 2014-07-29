e025522e9221d9233e4648866a62e615ac766dfe
peterwl <peterwl76@yahoo.com>
2014-07-29 15:43:02 -0400

    added frames.length to .pop

--- components/sam/js/sp_postSam.js
+++ components/sam/js/sp_postSam.js
@@ -339,7 +339,7 @@
             });
              function resetFrames(){
                 playing = false;
-                frames = frames.pop();
+                frames = frames.pop(frames.length);
                 submittedFrames = frames.length-1;
                 framesIndicator.innerText = "frames.length-1/"+String(maxFrames);
                 overlay.getContext("2d").clearRect( 0, 0, width, height );
